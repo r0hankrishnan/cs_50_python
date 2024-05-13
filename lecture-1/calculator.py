bbad6804d7c9701375fa28ff57fdc946c95ae8e2
r0hankrishnan <rohan.krishnan.2020@gmail.com>
2024-05-12 21:18:37 -0400

    Update calculator.py

--- lecture-1/calculator.py
+++ lecture-1/calculator.py
@@ -1,18 +1,8 @@
-#Using ints
+def main():
+    x = int(input("x: "))
+    print("x squared is", square(x))
 
-#Declare variables (make x and y dynamic and convert to int from str)
-x = int(input("x value: "))
-y = int(input("y value: "))
+def square(n):
+    return n*n
 
-#Print sum
-print(x + y)
-
-#Using floats
-
-#Declare variables and conver to float from str
-a = float(input("a: "))
-b = float(input("b: "))
-
-#Print sum
-print(a + b)
-
+main()
