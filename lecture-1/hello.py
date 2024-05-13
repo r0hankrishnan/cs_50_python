62b3d909b1892478bb59fba69917df2060a951db
r0hankrishnan <rohan.krishnan.2020@gmail.com>
2024-05-12 20:27:38 -0400

    Add str code example
    From start of lecture until  1:03:07.

--- lecture-1/hello.py
+++ lecture-1/hello.py
@@ -1,1 +1,16 @@
-print("hello, world")
+#Working with inputs, strings, and methods
+
+#Ask for name and store in a variable
+name = input("what is your name? ")
+
+#Remove white space from start and end and capitalize
+name = name.strip().title()
+
+#Split into first and last name
+first,last = name.split(" ")
+
+#Output a message that says "hello, " and the input name
+print("hello,", name) 
+
+#Output a message that tells you your first and last name
+print("Your first name is", first, "and your last name is", last)
