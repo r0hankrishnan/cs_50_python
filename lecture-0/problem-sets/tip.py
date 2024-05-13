600b4da1af277004dde36d483acd7beb2f3c5618
r0hankrishnan <rohan.krishnan.2020@gmail.com>
2024-05-13 03:04:54 -0400

    Update tip.py

--- lecture-0/problem-sets/tip.py
+++ lecture-0/problem-sets/tip.py
@@ -1,17 +1,19 @@
+#Main function
 def main():
     dollars = dollars_to_float(input("How much was the meal? "))
     percent = percent_to_float(input("What percentage would you like to tip? "))
     tip = dollars * percent
     print(f"Leave ${tip:.2f}")
 
-
+#Take string and replace $ with nothing then convert to float
 def dollars_to_float(d):
     cost = d.replace ("$", "")
     return float(cost)
 
-
+#Take string and replace % with nothing then covert to float and divide by 100
 def percent_to_float(p):
     percent = p.replace ("%", "")
     return float(percent)/100
 
+#Call main
 main()
