a653f7d000f1c09686f9ef766aa069a862e1fc8c
r0hankrishnan <rohan.krishnan.2020@gmail.com>
2024-05-12 21:05:24 -0400

    Update calculator.py

--- lecture-1/calculator.py
+++ lecture-1/calculator.py
@@ -1,7 +1,18 @@
+#Using ints
+
 #Declare variables (make x and y dynamic and convert to int from str)
 x = int(input("x value: "))
 y = int(input("y value: "))
 
 #Print sum
 print(x + y)
+
+#Using floats
+
+#Declare variables and conver to float from str
+a = float(input("a: "))
+b = float(input("b: "))
+
+#Print sum
+print(a + b)
 
