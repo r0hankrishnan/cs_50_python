250c8b17ffbf4672c9ebaf6f8c820f98486bfcf1
r0hankrishnan <rohan.krishnan.2020@gmail.com>
2024-05-18 23:44:27 -0400

    Update bank.py

--- lecture-1/problem-sets/bank.py
+++ lecture-1/problem-sets/bank.py
@@ -1,11 +1,16 @@
-
-greeting = "hello there"
-
 #If greeting contains hello, return 0
 #If greeting starts with h, return $20
 #Else return $100
+def main():
+    greeting = input("Greeting: ")
+    check_greeting(greeting)
 
+def check_greeting(x):
+    if x.find("hello") == 0:
+        print("$0")
+    elif x.find("h") == 0: 
+        print("$20")
+    else:
+        print("$100")
 
-x = greeting.rfind("hello")
-
-x
+main()
