87630bed4f5869a35491735de63b1c04aa519bd0
r0hankrishnan <rohan.krishnan.2020@gmail.com>
2024-05-20 00:50:38 -0400

    Update coke.py

--- lecture-2/problem-sets/coke.py
+++ lecture-2/problem-sets/coke.py
@@ -1,7 +1,26 @@
-total = 0
-while total <= 50:
-    
+#Prompts user to enter a coin one at a time and inform amount due
+#Only accept 25, 10, or 5 cent coins
+#Once user inputs at least 50 cents, ouput how many cents in change they are owed
+#Ignore any integer that isn't 25,10, or 5
+
+def get_cents():
     while True:
-        amt = int(input("Insert coin"))
-        if amt == 5 or amt -= 10 or amt ==25:
+        print("Please input either 5, 10, or 25 cent values")
+        amt = int(input("Insert coin: "))
+        if amt == 5 or amt == 10 or amt ==25:
             break
+    return amt
+
+def calc_change():
+    total = 0
+    while total < 50:
+        amt = get_cents()
+        total = total + amt
+        if total < 50:
+            print(f"You have", 50 - total, "cents left to pay")
+        else: 
+            break
+
+    print(f"You are owed", total-50, "cents")
+
+calc_change()
